a20cf453d53cc892cd720eaef63425d30c1af72a
Michele-Brignola <michele.brignola@hotmail.com>
2026-01-16 17:21:52 +0100

    added snack2

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ const biciclette = [
 
 biciclette.sort((a, b) => a.peso_kg - b.peso_kg);
 console.log(biciclette[1].peso_kg);
+
+//* Snack 2
+
+const squadre = [
+  { nome: "Juventus", punti: 0, falli: 0 },
+  { nome: "Milan", punti: 0, falli: 0 },
+  { nome: "Inter", punti: 0, falli: 0 },
+  { nome: "Napoli", punti: 0, falli: 0 },
+];
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let scquadra of squadre) {
+  scquadra.punti = randomNumber(10, 30);
+  scquadra.falli = randomNumber(2, 10);
+}
+
+console.table(squadre)
